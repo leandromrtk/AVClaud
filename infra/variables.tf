f80eba70abb03f3956fdeb477b0449d8c9db78c7
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2025-04-10 01:00:23 -0300

    Delete infra directory

--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  description = "Localização dos recursos"
-  default     = "East US"
-}
-
-variable "resource_group_name" {
-  description = "Nome do grupo de recursos"
-  default     = "rg-restaurante"
-}
